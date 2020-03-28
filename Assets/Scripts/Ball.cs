d9647af82ac97717002947ac727d61040b40d8f4
Rashadat Mirzayev <334rashad@gmail.com>
2020-03-28 17:47:27 +0100

    start game configs

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,19 +1,49 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    // config params
     [SerializeField] Paddle paddle1;
+    [SerializeField] Vector2 paddlePosition;
+    [SerializeField] float velocityX = 2f;
+    [SerializeField] float velocityY = 15f;
+    bool hasStarted = false;
+
+    // state
+    Vector2 paddleToBallVector;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        paddleToBallVector = transform.position - paddle1.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchBallOnClick();
+        }
+
+    }
+
+    private void LaunchBallOnClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(velocityX, velocityY);
+            hasStarted = true;
+        } 
+    }
+
+    private void LockBallToPaddle()
+    {
+        paddlePosition = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePosition + paddleToBallVector;
     }
 }
