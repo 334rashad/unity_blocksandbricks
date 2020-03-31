7f664d3e526e92de1443b693cccaf4766392415d
Rashadat Mirzayev <334rashad@gmail.com>
2020-03-31 23:48:08 +0200

    counting blocks

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -6,6 +6,15 @@ public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip destroySound;
 
+    //cached reference
+    Level level;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CountRemainingBlocks();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         AudioSource.PlayClipAtPoint(destroySound, Camera.main.transform.position);
